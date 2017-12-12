82fb4007aaa0484e31ebdc56a6744017ab94c18f
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-12 15:09:42 +1000

    Add route caching

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as routeCache from 'route-cache'
 import { addRoute } from './routes/add'
 import { subtractRoute } from './routes/subtract'
 import { multiplyRoute } from './routes/multiply'
@@ -6,11 +7,14 @@ import { divideRoute } from './routes/divide'
 
 const app = express()
 
+const CACHE_SECONDS = 60
+const cache = routeCache.cacheSeconds(CACHE_SECONDS)
+
 app.use(express.static('www'))
 
-app.get('/add/:x/:y?', addRoute)
-app.get('/subtract/:x/:y?', subtractRoute)
-app.get('/multiply/:x:y?', multiplyRoute)
-app.get('/divide/:x/:y?', divideRoute)
+app.get('/add/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), addRoute)
+app.get('/subtract/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), subtractRoute)
+app.get('/multiply/:x:y?', routeCache.cacheSeconds(CACHE_SECONDS), multiplyRoute)
+app.get('/divide/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), divideRoute)
 
 app.listen(3000, () => console.log('Listening on Port 3000'))
