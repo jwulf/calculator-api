9bb610aec82471703d9731a48d8ba8dfd80219db
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-12 20:17:05 +1000

    Linting

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ const app = express()
 
 app.use(express.static('www'))
 
-Routes.forEach(route => app.get(
-    `${route.href}/:x/:y?`,
-    routeCache.cacheSeconds(CACHE_SECONDS),
-    route.handler
-))
+Routes.forEach(route =>
+    app.get(
+        `${route.href}/:x/:y?`,
+        routeCache.cacheSeconds(CACHE_SECONDS),
+        route.handler))
 
 app.listen(3000, () => console.log('Listening on Port 3000'))
