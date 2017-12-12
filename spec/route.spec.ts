af6f60ae452d8c3385208a8e2c99d84b7f5c3c26
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-12 16:48:28 +1000

    Extend tests

--- spec/route.spec.ts
+++ spec/route.spec.ts
@@ -1,10 +1,36 @@
 import * as Route from '../src/routes/routes'
 
+console.log('asdfa')
 describe('add', () => {
-
     const add = Route.responseBuilder(Route.fns.addFn)
-    it('returns a list of links', () => {
-        const res = add(4)
-        expect(res.currentTotal).toBe(4)
-    })
+    it('returns a list of links',
+        () => expect(add(4).links.length).toBe(4))
+    it('returns a currentTotal',
+        () => expect(add(4).currentTotal).toBeTruthy())
+    it('returns the correct current total for a single arg',
+        () => expect(add(6).currentTotal).toBe(6))
+    it('returns the correct current total for a two args',
+        () => expect(add(6, 9).currentTotal).toBe(15)),
+    it('returns a list of links with two args',
+        () => expect(add(7, -8).links.length).toBe(4))
+})
+
+describe('subtractFn', () => {
+    const subtractFn = Route.fns.subtractFn
+    expect(subtractFn(4, 2)).toBe(2)
+})
+
+describe('subtract', () => {
+
+    const subtract = Route.responseBuilder(Route.fns.subtractFn)
+    it('returns a list of links',
+        () => expect(subtract(4).links.length).toBe(4))
+    it('returns a currentTotal',
+        () => expect(subtract(4).currentTotal).toBeTruthy())
+    it('returns the correct current total for a single arg',
+        () => expect(subtract(6).currentTotal).toBe(6))
+    it('returns the correct current total for a two args',
+        () => expect(subtract(6, 9).currentTotal).toBe(6 - 9)),
+    it('returns a list of links with two args',
+        () => expect(subtract(7, -8).links.length).toBe(7 - -8))
 })
