eebe7acfe93e64e2c4eca01b7fdad2ab8cdb7fb0
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-12 16:49:31 +1000

    Fix failing test

--- spec/route.spec.ts
+++ spec/route.spec.ts
@@ -15,13 +15,7 @@ describe('add', () => {
         () => expect(add(7, -8).links.length).toBe(4))
 })
 
-describe('subtractFn', () => {
-    const subtractFn = Route.fns.subtractFn
-    expect(subtractFn(4, 2)).toBe(2)
-})
-
 describe('subtract', () => {
-
     const subtract = Route.responseBuilder(Route.fns.subtractFn)
     it('returns a list of links',
         () => expect(subtract(4).links.length).toBe(4))
@@ -32,5 +26,5 @@ describe('subtract', () => {
     it('returns the correct current total for a two args',
         () => expect(subtract(6, 9).currentTotal).toBe(6 - 9)),
     it('returns a list of links with two args',
-        () => expect(subtract(7, -8).links.length).toBe(7 - -8))
+        () => expect(subtract(7, -8).links.length).toBe(4))
 })
