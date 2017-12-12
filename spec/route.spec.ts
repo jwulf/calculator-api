f6135839e7d6959d9b5a469d173553394dd5e5c2
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-13 00:44:17 +1000

    Fixed failing test

--- spec/route.spec.ts
+++ spec/route.spec.ts
@@ -68,7 +68,7 @@ describe('link builder', () => {
     const X = 5
     const Y = 6
     it('returns the correct add link for one arg',
-        () => expect(multiply(X).links.filter(link => link.href === `${Routes.add.href}/${X}/`).length).toBe(1))
+        () => expect(multiply(X).links.filter(link => link.href === `${Routes.add.href}/${X}`).length).toBe(1))
     it('returns the correct add link for two args',
-        () => expect(multiply(X, Y).links.filter(link => link.href === `${Routes.add.href}/${X * Y}/`).length).toBe(1))
+        () => expect(multiply(X, Y).links.filter(link => link.href === `${Routes.add.href}/${X * Y}`).length).toBe(1))
 })
