1412594356804e466a210194e2c1f24eb25b8cce
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-13 01:05:30 +1000

    Add HTTP 400 for invalid input

--- src/routes/route-logic.ts
+++ src/routes/route-logic.ts
@@ -4,6 +4,10 @@ export const buildRouteHandler = HATEOASResponder => (req, res) => {
     const { x, y } = req.params
     const numX = x ? parseFloat(x) : undefined
     const numY = y ? parseFloat(y) : undefined
+    if ((numX !== undefined && isNaN(numX)) || (numY !== undefined && isNaN(numY))) {
+        res.status(400)
+        return res.send('Invalid input')
+    }
     res.send(HATEOASResponder(numX, numY))
 }
 
