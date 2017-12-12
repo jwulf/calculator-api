bca6bde82d2e9a0e25ee2e4109094a74424a74c5
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-12 15:42:02 +1000

    refactor RoutePaths for type safety

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ import { addRoute } from './routes/add'
 import { subtractRoute } from './routes/subtract'
 import { multiplyRoute } from './routes/multiply'
 import { divideRoute } from './routes/divide'
+import { RoutePaths } from './routes/route-paths'
 
 const app = express()
 
 const CACHE_SECONDS = 60
-const cache = routeCache.cacheSeconds(CACHE_SECONDS)
 
 app.use(express.static('www'))
 
-app.get('/add/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), addRoute)
-app.get('/subtract/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), subtractRoute)
-app.get('/multiply/:x:y?', routeCache.cacheSeconds(CACHE_SECONDS), multiplyRoute)
-app.get('/divide/:x/:y?', routeCache.cacheSeconds(CACHE_SECONDS), divideRoute)
+app.get(`${RoutePaths.add}:x/:y?`, routeCache.cacheSeconds(CACHE_SECONDS), addRoute)
+app.get(`${RoutePaths.subtract}/:x/:y?`, routeCache.cacheSeconds(CACHE_SECONDS), subtractRoute)
+app.get(`${RoutePaths.multiply}/:x:y?`, routeCache.cacheSeconds(CACHE_SECONDS), multiplyRoute)
+app.get(`${RoutePaths.divide}/:x/:y?`, routeCache.cacheSeconds(CACHE_SECONDS), divideRoute)
 
 app.listen(3000, () => console.log('Listening on Port 3000'))
---
+++ src/routes/route-paths.ts
@@ -0,0 +1,6 @@
+export const RoutePaths = {
+    add: '/add',
+    subtract: '/subtract',
+    multiply: '/multiply',
+    divide: '/divide'
+}
