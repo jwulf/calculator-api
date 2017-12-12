851c63c83acd51cb16e6d80f7563ef9e5ccc5526
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-13 00:44:26 +1000

    Removed a line!

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,5 @@ import { RouteConfig } from './route-config'
 
 export const Routes = RouteConfig.map(route => ({
     href: route.href,
-    response: buildHATEOASResponder(route.fn),
     handler: buildRouteHandler(buildHATEOASResponder(route.fn))
 }))
